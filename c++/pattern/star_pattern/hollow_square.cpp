98a149191e691c0fbb7d10c18fab6e1f2796ae15
Asharay Paliwal <0808al211012.ies@ipsacademy.org>
2025-07-04 16:02:03 +0530

    completed hollow square

---
+++ c++/pattern/star_pattern/hollow_square.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter your solid star pattern size = ";
+    int i,k,n;
+    cin>>n;
+    for(i=0;i<n;i++){
+        for(k=0;k<n;k++){
+          if(i>0&&i<n-1&&k>0&&k<n-1)
+          cout<<" ";
+          else{
+            cout<<"*";
+           }
+         }
+        cout<<"\n";
+    }
+    return 0;
+}
