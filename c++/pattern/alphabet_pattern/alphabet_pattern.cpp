84ebda4253ef24edcfd51277e5efb9d490fa1146
Asharay Paliwal <0808al211012.ies@ipsacademy.org>
2025-07-10 16:42:36 +0530

    Completed equilateral triangle

--- c++/pattern/alphabet_pattern/alphabet_pattern.cpp
+++ c++/pattern/alphabet_pattern/alphabet_pattern.cpp
@@ -1,20 +1,33 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int n,i,j;
-    cin>>n;
-    for(i=97;i<97+n;i++){
-        for(j=97;j<=i;j++){
-            if(j>=123){
-                int l=j-26;
-                cout<<char(l)<<char(l);
-            }
-            else{
-                cout<<char(j);
-            }
+int main()
+{
+    int i, j, n = 5;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            cout << (char)('A' + j - 1) << " ";
         }
-        cout<<"\n";
+        cout << endl;
     }
     return 0;
 }
+
+// int main(){
+//     int n,i,j;
+//     cout<<"Enter your triangle size= ";
+//     cin>>n;
+//     for(i=97;i<97+n;i++){
+//         for(j=97;j<=i;j++){
+//             if(j>=123){
+//                 int l=j-26;
+//                 cout<<char(l)<<char(l);
+//             }
+//             else{
+//                 cout<<char(j);
+//             }
+//         }
+//         cout<<"\n";
+//     }
+//     return 0;
+// }
